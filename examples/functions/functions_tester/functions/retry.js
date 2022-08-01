b22af05ca3837acc212a9d4d7601f546086732c4
Ben Perlmutter <90647379+mongodben@users.noreply.github.com>
2022-08-01 17:26:48 -0400

    remove unnec files

--- examples/functions/functions_tester/functions/retry.js
+++
@@ -1,4 +0,0 @@
-async function retry({ fullDocument: logEntry }) {
-  const { args, attemptNumber, functionName, operationId } = logEntry;
-  context.functions.execute(functionName, ...args, attemptNumber, operationId);
-}
