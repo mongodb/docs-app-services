d4ad2bdad67ca3a6d6f1239b04d3891649f8697e
Ben Perlmutter <90647379+mongodben@users.noreply.github.com>
2022-09-21 09:31:04 -0400

    stub out materialize integration test

--- examples/app-services-examples-app/test/integration/materializeMonthlyProductSales.test.js
+++ examples/app-services-examples-app/test/integration/materializeMonthlyProductSales.test.js
@@ -11,9 +11,12 @@ describe("Materialize monthly product sales", () => {
   test("Should materialize sales", async () => {
     const sales = client.db("store").collection("sales");
 
-    const pizzaSale1 = {};
-    const pizzaSale2 = {};
+    const productId = "pizza";
+    const amountUsd = 3;
+    const timeOfSale2 = new Date(2022, 9, 15);
+    const timeOfSale1 = new Date(2022, 9, 20);
+    const pizzaSale1 = { productId, timeOfSale: timeOfSale1, amountUsd };
+    const pizzaSale2 = { productId, timeOfSale: timeOfSale2, amountUsd };
     await sales.insertMany([pizzaSale1, pizzaSale2]);
-    // TEST REQUIRES MANUAL VALIDATION ON PHONE THAT RECEIVES THE TEXT
   });
 });
