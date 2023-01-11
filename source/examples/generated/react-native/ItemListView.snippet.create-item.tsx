a3e760d10ed8a74382f7bdd126129e87012ff0f9
krollins-mdb <kyle.rollins@mongodb.com>
2023-01-11 09:48:56 -0600

    Add updated code sample

--- source/examples/generated/react-native/ItemListView.snippet.create-item.tsx
+++ source/examples/generated/react-native/ItemListView.snippet.create-item.tsx
@@ -1,6 +1,5 @@
 const createItem = useCallback(
   ({summary, priority}: {summary: string, priority: string}) => {
-    // if the realm exists, create an Item
     realm.write(() => {
       return new Item(realm, {
         summary,
