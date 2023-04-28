e9a27323cba917b9e3d56ff23bff1c8e8d2daded
Nick Larew <nick.larew@mongodb.com>
2023-04-28 14:11:58 -0400

    Move openapi-data-api-v1 directory out of source

--- openapi-data-api-v1/preview-bundled.sh
+++ openapi-data-api-v1/preview-bundled.sh
@@ -1,5 +1,5 @@
 cd "$(dirname "$0")"
 
-BUNDLED_OPENAPI_FILE="./bundled.yaml"
+BUNDLED_OPENAPI_FILE="../openapi-data-api-v1.bundled.yaml"
 
 npx @redocly/cli@latest preview-docs "$BUNDLED_OPENAPI_FILE"
