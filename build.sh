75492a4e0645c1baca8823d0d08c8ff75077066c
Brandon Ly <brandonly@lostcoding.com>
2024-10-07 14:44:30 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,4 +2,8 @@ curl https://raw.githubusercontent.com/mongodb/docs-worker-pool/netlify-poc/scri
 sh build-site.sh
 
 
-git clone --depth 1 --filter=tree:0 https://github.com/mongodb/docs-worker-pool.git --sparse
+# if [ ! -d "docs-worker-pool" ]; then
+  git clone --depth 1 --filter=tree:0 https://github.com/mongodb/docs-worker-pool.git --sparse
+  cd docs-worker-pool && git sparse-checkout set --no-cone modules/oas-page-builder
+  cd modules/oas-page-builder && npm ci && npm run build
+#fi 
