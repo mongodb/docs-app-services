3ea251b246af61425b710bfb4f640111670abb5f
Brandon Ly <brandonly@lostcoding.com>
2024-10-07 14:39:13 -0500

    Hotfix for OpenAPI

--- build.sh
+++ build.sh
@@ -1,2 +1,5 @@
 curl https://raw.githubusercontent.com/mongodb/docs-worker-pool/netlify-poc/scripts/build-site.sh -o build-site.sh 
 sh build-site.sh
+
+
+git clone --depth 1 --filter=tree:0 https://github.com/mongodb/docs-worker-pool.git --sparse
